ec017077561d610398a51d255984b689f396d34c
Syrenyx <paul@stagl.at>
2022-09-25 17:21:05 +0200

    Hidden Strawberry & Plat Berry Collect Trigger fully implemented

--- Loenn/entities/hiddenStrawberry.lua
+++ Loenn/entities/hiddenStrawberry.lua
@@ -5,7 +5,7 @@ HiddenStrawberry.depth = 8998
 HiddenStrawberry.texture = "collectables/ghostberry/idle00"
 HiddenStrawberry.justification = {0, 0}
 HiddenStrawberry.placements = {
-    name = "hiddenStrawberry",
+    name = "default",
 }
 
 return HiddenStrawberry
